05d344df4a62dd4ea699c75e7adeb555d0689395
Jane A Lieviant <jlieviant@gmail.com>
2022-01-10 12:51:49 +0700

    Initial commit with Songlin's build

---
+++ app/src/main/java/com/example/ca/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.ca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(20);
+
+        Button btn = findViewById(R.id.btnSubmitUrl);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText et = v.getRootView().findViewById(R.id.txtUrl);
+                String url = et.getText().toString();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String html = getHtml(url);
+                        ArrayList<String> pics = (ArrayList<String>) getImgSrc(html);
+                        Bitmap[] fetched = new Bitmap[20];
+                        for(Bitmap bm: fetched){
+                            bm = null;
+                        }
+                        int num = 0;
+                        for(String pic: pics){
+                            Bitmap bitmap = getBitmapFromURL(pic);
+                            if(bitmap != null){
+                                fetched[num] = bitmap;
+                                num++;
+                                runOnUiThread(new myRunnable(num, v, fetched, progressBar));
+                                if(num == 20){
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }).start();
+
+            }
+        });
+    }
+
+    public static List<String> getImgSrc(String htmlStr) {
+        if( htmlStr == null ){
+            return null;
+        }
+        String img = "";
+        Pattern p_image;
+        Matcher m_image;
+        List<String> pics = new ArrayList<String>();
+        String regEx_img = "<img.* src=\\s*(.*?)[^>]*?>";
+        p_image = Pattern.compile(regEx_img, Pattern.CASE_INSENSITIVE);
+        m_image = p_image.matcher(htmlStr);
+        while (m_image.find()) {
+            img = m_image.group();
+            Matcher m = Pattern.compile(" src\\s*=\\s*\"?(.*?)(\"|>|\\s+)").matcher(img);
+            while (m.find()) {
+                pics.add(m.group(1));
+            }
+        }
+        return pics;
+    }
+
+    public String getHtml(String urlString){
+        String html = "";
+        try {
+            URL url = new URL(urlString);
+            try {
+                InputStream is = url.openStream();
+                InputStreamReader isr = new InputStreamReader(is,"utf-8");
+
+                BufferedReader br = new BufferedReader(isr);
+                String data = br.readLine();
+                while (data != null){
+                    html = html + data;
+                    data = br.readLine();
+                }
+                br.close();
+                isr.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return html;
+    }
+
+    public Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/ca/myRunnable.java
@@ -0,0 +1,100 @@
+package com.example.ca;
+
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class myRunnable implements Runnable{
+    private int i;
+    private View v;
+    private Bitmap[] fetched;
+    private ProgressBar progressBar;
+
+    public myRunnable(int i, View v, Bitmap[] fetched, ProgressBar progressBar){
+        this.i = i;
+        this.v = v;
+        this.fetched = fetched;
+        this.progressBar = progressBar;
+    }
+
+    @Override
+    public void run(){
+        ImageView iv = getImageView(v, i);
+        iv.setImageBitmap(fetched[i-1]);
+        progressBar.setProgress(i);
+        TextView tv = v.getRootView().findViewById(R.id.txtProgress);
+        tv.setText(i + " out of 20 downloaded");
+    }
+
+    public  ImageView getImageView(View v, int i){
+        ImageView iv = null;
+        switch (i){
+            case 1:
+                iv = v.getRootView().findViewById(R.id.img1);
+                break;
+            case 2:
+                iv = v.getRootView().findViewById(R.id.img2);
+                break;
+            case 3:
+                iv = v.getRootView().findViewById(R.id.img3);
+                break;
+            case 4:
+                iv = v.getRootView().findViewById(R.id.img4);
+                break;
+            case 5:
+                iv = v.getRootView().findViewById(R.id.img5);
+                break;
+            case 6:
+                iv = v.getRootView().findViewById(R.id.img6);
+                break;
+            case 7:
+                iv = v.getRootView().findViewById(R.id.img7);
+                break;
+            case 8:
+                iv = v.getRootView().findViewById(R.id.img8);
+                break;
+            case 9:
+                iv = v.getRootView().findViewById(R.id.img9);
+                break;
+            case 10:
+                iv = v.getRootView().findViewById(R.id.img10);
+                break;
+            case 11:
+                iv = v.getRootView().findViewById(R.id.img11);
+                break;
+            case 12:
+                iv = v.getRootView().findViewById(R.id.img12);
+                break;
+            case 13:
+                iv = v.getRootView().findViewById(R.id.img13);
+                break;
+            case 14:
+                iv = v.getRootView().findViewById(R.id.img14);
+                break;
+            case 15:
+                iv = v.getRootView().findViewById(R.id.img15);
+                break;
+            case 16:
+                iv = v.getRootView().findViewById(R.id.img16);
+                break;
+            case 17:
+                iv = v.getRootView().findViewById(R.id.img17);
+                break;
+            case 18:
+                iv = v.getRootView().findViewById(R.id.img18);
+                break;
+            case 19:
+                iv = v.getRootView().findViewById(R.id.img19);
+                break;
+            case 20:
+                iv = v.getRootView().findViewById(R.id.img20);
+                break;
+            default:
+                break;
+        }
+        return iv;
+    }
+
+}
