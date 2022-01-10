77e37f3d55c304923de6557409ee3e86c554b31e
joshualeow <joshualeowzhihao@gmail.com>
2022-01-10 21:04:08 +0800

    Added TextView and Timer to SecondActivity

--- app/src/main/java/com/example/ca/SecondActivity.java
+++ app/src/main/java/com/example/ca/SecondActivity.java
@@ -1,14 +1,35 @@
 package com.example.ca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.ListFragment;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Chronometer;
 
-public class SecondActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SecondActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //Second activity - also has a grid but it is now 3x4
+        //
+        Chronometer simpleChronometer = (Chronometer) findViewById(R.id.timer); // initiate a chronometer
+        simpleChronometer.start(); // start a chronometer
+
+        //Only 6 images passed to 2nd activity, allow users to choose
+        //Show that image has been selected.
+        //
+        ArrayList<Bitmap> images = MainActivity.selected;
+
+
+        //if selected same, both will stay as it is
+        //
+        //When all of them matches, bring user back to main activity
+
     }
 }
